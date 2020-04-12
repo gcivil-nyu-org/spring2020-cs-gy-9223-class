52340f36c75bc9c386df5c398637ee1eb7fa2230
VentusXu09 <ventusxu0905@gmail.com>
2020-04-12 15:25:24 -0400

    add customized JSONField Serializer

--- mercury/views/measurement.py
+++ mercury/views/measurement.py
@@ -14,9 +14,9 @@ def build_error(str):
 
 def add_measurement(request, event):
     json_data = request.data
-    # TODO: Abandon str as type of json_data in Sprint 6
-    if isinstance(json_data, str):
-        json_data = json.loads(json_data)
+    # # TODO: Abandon str as type of json_data in Sprint 6
+    # if isinstance(json_data, str):
+    #     json_data = json.loads(json_data)
 
     res = {"event_uuid": event.uuid}
     key_map = {"timestamp": "date", "sensor_id": "sensor_id", "value": "values"}
@@ -28,12 +28,6 @@ def add_measurement(request, event):
                 status=status.HTTP_400_BAD_REQUEST,
             )
         res[key] = json_data[json_key]
-
-    # TODO: Abandon str as type of json_data in Sprint 6
-    # SQL query fails to select values from
-    # JSONField if the str is stored in it
-    if isinstance(res["value"], str):
-        res["value"] = json.loads(res["value"])
 
     serializer = AGMeasurementSerializer(data=res)
     try:
